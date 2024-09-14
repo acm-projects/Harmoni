3ed254877d570e2ce697d7ecf164ec8685067746
aymanhaque <muhd.ayman.haque@gmail.com>
2024-09-13 22:46:32 -0500

    Some minor addons. Nothing Major

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require('express'); //Ayman's Changes
+const { MongoClient } = require('mongodb'); //Ayman's Changes
+const dotenv = require("dotenv") //Ayman's Changes
+dotenv.config(); //Ayman's Changes
 
 const app = express()
+
+app.use(cors()); //Ayman's Changes
+app.use(express.json()); //Ayman's Changes
 
 app.get('/', (req,res) => {
     res.send('Hello World')
@@ -9,3 +15,4 @@ app.get('/', (req,res) => {
 app.listen(8000, () => {
     console.log('port is listening');
 })
+
