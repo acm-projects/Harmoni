32bec14b5d45f4fd69f442d61cb23d8bb1242df7
aymanhaque <muhd.ayman.haque@gmail.com>
2024-09-13 23:22:36 -0500

    "FInished setting up MongoDb code. Just need permissions to connect MongoDB"

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,9 +1,12 @@
-const express = require('express'); //Ayman's Changes
 const { MongoClient } = require('mongodb'); //Ayman's Changes
 const dotenv = require("dotenv") //Ayman's Changes
-dotenv.config(); //Ayman's Changes
 
-const app = express()
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const port = 8000;
+dotenv.config(); //Ayman's Changes
 
 app.use(cors()); //Ayman's Changes
 app.use(express.json()); //Ayman's Changes
@@ -13,6 +16,26 @@ app.get('/', (req,res) => {
 })
 
 app.listen(8000, () => {
-    console.log('port is listening');
+    try {
+        console.log("Port is listening :)");
+    } catch(e) {
+        console.log("Port is not listening :(")
+    }
 })
 
+
+
+async function runMongo(){
+    const MONGO_URI = "mongodb+srv://kartikjoshiron:<db_password>@harmonidatabase.f9go7.mongodb.net/?retryWrites=true&w=majority&appName=HarmoniDatabase"
+    const dataBase = new MongoClient(MONGO_URI)
+    
+    try {
+        await dataBase.connect();
+        console.log("Database is connnected! :) ")
+    } catch(e) {
+        console.log("Database not connected :(")
+    }
+}
+
+
+runMongo();
