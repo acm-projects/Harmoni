6f8407c27238e033c1f219b9cde1b63303c7e8f4
aymanhaque <muhd.ayman.haque@gmail.com>
2024-09-16 23:40:53 -0500

    Connected to mongodb database

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,21 +1,22 @@
-const { MongoClient } = require('mongodb'); //Ayman's Changes
-const dotenv = require("dotenv") //Ayman's Changes
+const { MongoClient } = require('mongodb'); 
+const dotenv = require("dotenv") 
+
 
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
 const port = 8000;
-dotenv.config(); //Ayman's Changes
+dotenv.config(); 
 
-app.use(cors()); //Ayman's Changes
-app.use(express.json()); //Ayman's Changes
+app.use(cors()); 
+app.use(express.json()); 
 
 app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
-app.listen(8000, () => {
+app.listen(port, () => {
     try {
         console.log("Port is listening :)");
     } catch(e) {
@@ -23,18 +24,32 @@ app.listen(8000, () => {
     }
 })
 
-
+//mongoPassword: JIOglWWCF4QRT3y4
 
 async function runMongo(){
-    const MONGO_URI = "mongodb+srv://kartikjoshiron:<db_password>@harmonidatabase.f9go7.mongodb.net/?retryWrites=true&w=majority&appName=HarmoniDatabase"
-    const dataBase = new MongoClient(MONGO_URI)
+    const MONGO_URI = "mongodb+srv://muhdaymanhaque:JIOglWWCF4QRT3y4@harmoni.etov0.mongodb.net/?retryWrites=true&w=majority&appName=Harmoni"
+    const client = new MongoClient(MONGO_URI)
     
     try {
-        await dataBase.connect();
-        console.log("Database is connnected! :) ")
+        await client.connect();
+        console.log("Database is connnected! :) ");
+
+        const dataBase = client.db('UserData');
+        const collection = dataBase.collection('Credentials(PasswordEmail)');
+
+        const pointer = collection.find({});
+
+        pointer.forEach(document => {
+            console.log(document);
+        })
+        
+
+
     } catch(e) {
-        console.log("Database not connected :(")
+        console.log("Database not connected :(");
     }
+
+    
 }
 
 
